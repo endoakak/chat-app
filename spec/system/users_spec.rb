f9f68890a5a32be6c6663371246eef5449c0c452
endoakak <akendo2020@gmail.com>
2020-07-20 17:06:13 +0900

    結合テストの実装

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'ログインしていない場合、サインページに移動する' do
+    # トップページに遷移する
+    visit root_path
+    # ログインしていない場合、サインインページに遷移することを期待する
+    expect(current_path).to eq new_user_session_path
+  end
+
+  it 'ログインに成功し、ルートパスに遷移する' do
+    # 予め、ユーザーをDBに保存する
+    @user = FactoryBot.create(:user)
+    # サインインページへ移動する
+    visit new_user_session_path
+    # ログインしていない場合、サインインページに遷移することを期待する
+    expect(current_path).to eq new_user_session_path
+    # すでに保存されているユーザーのemailとpasswordを入力する
+    fill_in "user_email", with: @user.email
+    fill_in "user_password", with: @user.password
+    # ログインボタンをクリックする
+    find('input[name="commit"]').click
+    # ルートページに遷移することを期待する
+    expect(current_path).to eq root_path
+  end
+  it 'ログインに失敗し、再びサインインページに戻ってくる' do
+    # 予め、ユーザーをDBに保存する
+    @user = FactoryBot.create(:user)
+    # サインインページへ移動する
+    visit new_user_session_path
+    # 誤ったユーザー情報を入力する
+    fill_in "user_email", with: "abcmail"
+    fill_in "user_password", with: "123@456"
+    # ログインボタンをクリックする
+    click_on("Log in")
+    # サインインページに遷移していることを期待する
+    expect(current_path).to eq new_user_session_path
+  end
+end
